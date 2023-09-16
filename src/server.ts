340fdc649e6efcb05ef65c2b2cb72015a7d9e06d
umesh <umesh37kr@gmail.com>
2023-09-16 09:18:23 +0530

    add eslint and husky

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,10 @@
-function login(user_name: string): boolean {
-    return true;
+function login(user_name: string): string {
+    // console.log("username")
+    const user = {
+        name: 'umesh',
+    };
+    const userdata = user.name;
+    return user_name + userdata;
 }
 
 login('umeshkr');
