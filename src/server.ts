bfb2397b8cfff728c1ae8138ecd003fa32f7cd74
umesh <umesh37kr@gmail.com>
2023-09-17 14:05:00 +0530

    add express js

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,18 @@
-// import { Config } from './config/index'
-// console.log(Config.PORT)
+import app from './app';
+import { Config } from './config/index';
+const PORT = Config.PORT;
+
+const startServer = () => {
+    try {
+        app.listen(PORT, () => {
+            // eslint-disable-next-line no-console
+            console.log(`server is listeing on ${PORT}`);
+        });
+    } catch (error) {
+        // eslint-disable-next-line no-console
+        console.error(error);
+        process.exit(1);
+    }
+};
+
+startServer();
