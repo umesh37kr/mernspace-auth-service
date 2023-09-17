c24a8d43c0846c35fb00dba6babd7b9e4be9a852
umesh <umesh37kr@gmail.com>
2023-09-17 22:47:53 +0530

    add logger

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,17 @@
 import app from './app';
 import { Config } from './config/index';
+import logger from './config/logger';
 const PORT = Config.PORT;
 
 const startServer = () => {
     try {
         app.listen(PORT, () => {
             // eslint-disable-next-line no-console
-            console.log(`server is listeing on ${PORT}`);
+            logger.info(`server is listeing on port ${PORT}`);
         });
     } catch (error) {
         // eslint-disable-next-line no-console
-        console.error(error);
+        logger.error(error);
         process.exit(1);
     }
 };
