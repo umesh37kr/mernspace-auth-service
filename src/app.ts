2873face292957b22252b0fe71c9856dfb7d86f4
umesh <umesh37kr@gmail.com>
2023-09-18 10:17:06 +0530

    add error handler

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,27 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import logger from './config/logger';
+import { HttpError } from 'http-errors';
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
     res.send('app is working ..');
 });
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+    logger.error(err.message);
+    const statusCode = err.statusCode || 500;
+    res.status(statusCode).json({
+        errors: [
+            {
+                statusCode: statusCode,
+                type: err.name,
+                message: err.message,
+                path: '',
+                location: '',
+            },
+        ],
+    });
+});
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -6,11 +6,9 @@ const PORT = Config.PORT;
 const startServer = () => {
     try {
         app.listen(PORT, () => {
-            // eslint-disable-next-line no-console
             logger.info(`server is listeing on port ${PORT}`);
         });
     } catch (error) {
-        // eslint-disable-next-line no-console
         logger.error(error);
         process.exit(1);
     }
