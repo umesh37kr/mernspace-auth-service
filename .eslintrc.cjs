8a00d18d127f269910a238431372051a07f0e49c
umesh <umesh37kr@gmail.com>
2023-09-18 10:18:30 +0530

    require await off

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,7 +15,7 @@ module.exports = {
     rules: {
         'no-console': 'error',
         'dot-notation': 'error',
-        '@typescript-eslint/no-misused-promises': "off",
-        '@typescript-eslint/require-await': "off"
+        '@typescript-eslint/no-misused-promises': 'off',
+        '@typescript-eslint/require-await': 'off',
     },
 };
