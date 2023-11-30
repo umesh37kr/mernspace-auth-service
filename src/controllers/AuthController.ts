3b31de295580efc5c793d928407e14399c532fc1
umesh <umesh37kr@gmail.com>
2023-11-30 09:26:42 +0530

    change status code of login end point

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -134,7 +134,7 @@ export class AuthController {
                 httpOnly: true, //very important
             });
             this.logger.info('User has been logged in', { id: user.id });
-            res.status(201).json({ id: user.id });
+            res.json({ id: user.id });
         } catch (error) {
             next(error);
             return;
