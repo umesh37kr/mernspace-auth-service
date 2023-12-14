a555d70952e66a1b5bee9e1c289255458cc57934
umesh <umesh37kr@gmail.com>
2023-12-14 12:22:05 +0530

    refresh token validation error fix

--- src/middlewares/validateRefreshToken.ts
+++ src/middlewares/validateRefreshToken.ts
@@ -1,5 +1,6 @@
 import { expressjwt } from 'express-jwt';
 import { Config } from '../config';
+import { Request } from 'express';
 import { AuthCookie, IRefreshTokenPayload } from '../types';
 import { AppDataSource } from '../config/data-source';
 import { RefreshToken } from '../entity/RefreshToken';
@@ -8,23 +9,22 @@ import logger from '../config/logger';
 export default expressjwt({
     secret: Config.REFRESH_TOKEN_SECRET!,
     algorithms: ['HS256'],
-    getToken(req) {
+    getToken(req: Request) {
         const { refreshToken } = req.cookies as AuthCookie;
         return refreshToken;
     },
-
-    async isRevoked(request, token) {
+    async isRevoked(request: Request, token) {
         try {
             const refreshTokenRepo = AppDataSource.getRepository(RefreshToken);
-            const refreshToken = refreshTokenRepo.findOne({
+            const refreshToken = await refreshTokenRepo.findOne({
                 where: {
                     id: Number((token?.payload as IRefreshTokenPayload).id),
                     user: { id: Number(token?.payload.sub) },
                 },
             });
             return refreshToken === null;
-        } catch (error) {
-            logger.error('error while getting refresh token', {
+        } catch (err) {
+            logger.error('Error while getting the refresh token', {
                 id: (token?.payload as IRefreshTokenPayload).id,
             });
         }
