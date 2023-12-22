24f107f64e0fcb01d13ed33f22cf7f70e9fd734d
umesh <umesh37kr@gmail.com>
2023-12-22 12:46:07 +0530

    fix error for creating tenant endpoint

--- src/routes/tenants.ts
+++ src/routes/tenants.ts
@@ -3,12 +3,13 @@ import { TenantController } from '../controllers/TenantController';
 import { TenantService } from '../services/TenantService';
 import { AppDataSource } from '../config/data-source';
 import { Tenant } from '../entity/Tenant';
+import logger from '../config/logger';
 
 const router = express.Router();
 
 const tenantRepository = AppDataSource.getRepository(Tenant);
 const tenantService = new TenantService(tenantRepository);
-const tenantController = new TenantController(tenantService);
+const tenantController = new TenantController(tenantService, logger);
 
 router.post('/', (req, res, next) => tenantController.create(req, res, next));
 
