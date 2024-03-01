ca0f0a5aca3aa3f761e608e2bbcbfefb0ac7dcc3
umesh <umesh37kr@gmail.com>
2024-03-01 09:07:19 +0530

    add tenant relation

--- src/middlewares/authenticate.ts
+++ src/middlewares/authenticate.ts
@@ -12,15 +12,17 @@ export default expressjwt({
     }) as GetVerificationKey,
     algorithms: ['RS256'],
     getToken(req: Request) {
-        const authHeader = req.headers.authorization;
-
+        // const authHeader = req.headers.authorization;
+        // console.log('authHeader ', authHeader);
+        // console.log('req.headers ', req.headers);
+        // console.log('Config.JWKS_URI! ', Config.JWKS_URI!);
         // Bearer eyjllsdjfljlasdjfljlsadjfljlsdf
-        if (authHeader && authHeader.split(' ')[1] !== 'undefined') {
-            const token = authHeader.split(' ')[1];
-            if (token) {
-                return token;
-            }
-        }
+        // if (authHeader && authHeader.split(' ')[1] !== 'undefined') {
+        //     const token = authHeader.split(' ')[1];
+        //     if (token) {
+        //         return token;
+        //     }
+        // }
 
         const { accessToken } = req.cookies as AuthCookie;
         return accessToken;
