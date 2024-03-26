9a42dc96e345ea4d593524639ca9fe971c0130a5
umesh <umesh37kr@gmail.com>
2024-03-26 13:23:29 +0530

    update tenant and email in update users api

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -42,8 +42,7 @@ export class UserController {
         if (!result.isEmpty()) {
             return res.status(400).json({ errors: result.array() });
         }
-
-        const { firstName, lastName, role } = req.body;
+        const { firstName, lastName, role, email, tenantId } = req.body;
         const userId = req.params.id;
 
         if (isNaN(Number(userId))) {
@@ -58,6 +57,8 @@ export class UserController {
                 firstName,
                 lastName,
                 role,
+                email,
+                tenantId,
             });
 
             this.logger.info('User has been updated', { id: userId });
--- src/validators/update-user-validator.ts
+++ src/validators/update-user-validator.ts
@@ -11,8 +11,21 @@ export default checkSchema({
         notEmpty: true,
         trim: true,
     },
+    email: {
+        isEmail: {
+            errorMessage: 'Invalid Email',
+        },
+        notEmpty: true,
+        errorMessage: 'Email is required!',
+        trim: true,
+    },
     role: {
         errorMessage: 'Role is required!',
+        notEmpty: true,
+        trim: true,
+    },
+    tenantId: {
+        errorMessage: 'tenant id is required!',
         notEmpty: true,
         trim: true,
     },
