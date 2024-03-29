5c888d6b1b9b63f4dbdbfa2398b675a411ef000c
umesh <umesh37kr@gmail.com>
2024-03-29 11:34:43 +0530

    fix tenantid optional if role admin

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -77,7 +77,7 @@ export class UserService {
                 lastName,
                 role,
                 email,
-                tenant: tenantId ? { id: tenantId } : undefined,
+                tenant: tenantId ? { id: tenantId } : null,
             });
         } catch (err) {
             const error = createHttpError(
